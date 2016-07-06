1b48f0644f4622f77e6489f259b8eb7ec388ba82
Hem Shrestha <hereshem@gmail.com>
2016-07-06 09:20:56 +0545

    firebase added

--- app/src/main/java/np/com/hemshrestha/college/AddStudentActivity.java
+++ app/src/main/java/np/com/hemshrestha/college/AddStudentActivity.java
@@ -1,7 +1,16 @@
 package np.com.hemshrestha.college;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AddStudentActivity extends AppCompatActivity {
 
@@ -10,7 +19,34 @@ public class AddStudentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_student);
 
-        saveStudent();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        String deviceId = Settings.System.getString(getContentResolver(),
+                Settings.System.ANDROID_ID);
+        DatabaseReference myRef = database.getReference(deviceId);
+
+        DatabaseReference myName = myRef.child("name");
+        myName.setValue("Hello, Worldetqwet!");
+
+        myRef.child("email").setValue("hello@myemail.com");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.child("name").getValue(String.class);
+                Log.d("firebase", "Value is: " + value);
+                Toast.makeText(getApplicationContext(), value, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("firebase", "Failed to read value.", error.toException());
+            }
+        });
+
+        //saveStudent();
     }
 
     private void saveStudent() {
